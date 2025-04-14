fa6ca1d1984459f6c7e24ec4b9ee05a0651c416e
Tristan Clark <tcjc107@gmail.com>
2025-04-13 18:06:35 -0700

    just switching my working branch to apropriate one

--- WeightTracker/DBInterface/DevWeightTrackerDBInterface.h
+++ WeightTracker/DBInterface/DevWeightTrackerDBInterface.h
@@ -1,24 +1,22 @@
 #ifndef DEV_WEIGHT_TRACKER_DB_INTERFACE_H
 #define DEV_WEIGHT_TRACKER_DB_INTERFACE_H
 
-#include "DBInterface.h"
+#include "FileIODBInterface.h"
 
 #include <fstream>
 #include <vector>
 #include <string>
 
-class DevWeightTrackerDBInterface : public  DBInterface
+class DevWeightTrackerDBInterface : public  FileIODBInterface
 {
     public:
         DevWeightTrackerDBInterface();
         bool connect() override;
-        bool disconnect() override;
-        void loadData() override;  // Load data from the database
-        void saveData() override;  // Save data to the database
-        void displayData() override;  // Display the data
-        std::vector<std::string> getData() override;
-        int getSize();
-        std::string getItem(const int index);
+        //bool disconnect() override;
+        //void loadData() override;  // Load data from the database
+        //void saveData() override;  // Save data to the database
+        //void displayData() override;  // Display the data
+        //std::vector<std::string> getData() override;
 
         void addItem(const std::string&);
     
